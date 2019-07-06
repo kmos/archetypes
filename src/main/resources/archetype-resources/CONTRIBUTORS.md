d1a44645350f643f4cba46462dea87272eba8a49
kmos <giovanni.panice@gmail.com>
2019-07-07 00:51:11 +0200

    first commit

---
+++ src/main/resources/archetype-resources/CONTRIBUTORS.md
@@ -0,0 +1,4 @@
+${projectName} contributors (sorted alphabetically)
+============================================
+
+* **[Giovanni Panice](https://github.com/kmos)**
---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -0,0 +1,15 @@
+package ${package};
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ApplicationTest{
+
+    @Test
+    void testMe() {
+
+    }
+
+}
